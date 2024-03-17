ab9dd0eba3e8bf8abce2356a26ce7eb0fd5f4964
fernandogbz <fgjv06@gmail.com>
2024-03-17 18:52:36 -0300

    Introducción a Java

---
+++ Condicionales.java
@@ -0,0 +1,12 @@
+public class Condicionales {
+    public static void main(String [] args){
+        double calificacion = 6.7;
+
+        if(calificacion >= 8.0){
+            System.out.println("Felicidades, aprobaste tu examen de Java");
+        }
+        else{
+            System.out.println("No aprobaste esta vez pero restan 2 intentos!");
+        }
+    }
+}
