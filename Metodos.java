eb51d1990bc4eb74090036d5a22f8d2c52a87bb7
fernandogbz <fgjv06@gmail.com>
2024-03-17 21:20:32 -0300

    Introducción a Java

---
+++ Metodos.java
@@ -0,0 +1,18 @@
+public class Metodos {
+  public static void main(String [] args){
+      int resultado = sumaDosNumeros(20, 50);
+      System.out.println(resultado);
+      resultado = sumaDosNumeros(100, 80);
+      System.out.println(resultado);
+      imprimeHola();
+  }
+
+  public static int sumaDosNumeros(int num1, int num2){
+      int total = num1 + num2;
+      return total;
+  }
+
+  public static void imprimeHola(){
+      System.out.println("¡Hola!");
+  }
+}
