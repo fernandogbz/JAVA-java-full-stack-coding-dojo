d19e5c35244bedd3469fc89d4681de949d686d04
fernandogbz <fgjv06@gmail.com>
2024-04-22 04:14:38 -0400

    Core Activities

--- Core-Activities/JAVA-core/15-Pokemon/pokemon/src/App.java
+++ Core-Activities/JAVA-core/15-Pokemon/pokemon/src/App.java
@@ -1,9 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
         Pokedex pokedex = new Pokedex();
-        Pokemon pikachu = pokedex.createPokemon("pikachi", 20, "xd");
-        Pokemon pikachu2 = pokedex.createPokemon("pikach2i", 20, "xd");
-        pikachu.attackPokemon(pikachu2);
+        Pokemon pikachu = pokedex.createPokemon("pikachu", 20, "xd");
+        Pokemon charmander = pokedex.createPokemon("charmander", 20, "xd");
+        pikachu.attackPokemon(charmander);
         pokedex.listPokemon();
     }
 }
